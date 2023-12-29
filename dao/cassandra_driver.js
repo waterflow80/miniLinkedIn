91fe5e1dc04cac2af8a1c22661ad31f6949bb948
waterflow80 <thessalonikaathena@outlook.com>
2023-12-29 01:38:53 +0100

    got some issues connecting to cassandra

--- dao/cassandra_driver.js
+++ dao/cassandra_driver.js
@@ -1,8 +1,9 @@
 const dotenv = require("dotenv");
-let cassandra = require("cassandra-driver");
+const cassandra = require("cassandra-driver");
+const logger = require("pino")();
 
 // dotenv init
-dotenv.config()
+dotenv.config();
 
 /**Cassandra instance implementing the singleton pattern */
 export default class CassandraDriver {
@@ -29,5 +30,52 @@ export default class CassandraDriver {
     } else {
       return CassandraDriver.instance;
     }
+  };
+
+  /**Transform an array of elements (strings) into one single string
+   * seperated by ',' (comma)
+   * @example ["id", "name", "age"] ==> "id, name, age"
+   */
+  arrayToString = function (arr) {
+    let result = "";
+    for (let elt of arr) {
+      result += elt + ", ";
+    }
+    return result.slice(0, result.length - 2);
+  };
+
+  /**Insert into the given table the provided values
+   * @param table the table name
+   * @param columns array that contain the names of the columns
+   * @param values the values to be inserted in each column
+   *
+   */
+  insertQuery = async function (query) {
+    let client = this.getInstance();
+    console.log("Query= " + query);
+
+    console.log("test");
+    await client.execute(query);
+    console.log("result: ", result);
+  };
+
+  /**Perform a SELECT * FROM table;
+   * @param table table name
+   *
+   * @returns rows | null
+   */
+  selectAllQuery = function (table) {
+    let client = this.getInstance();
+    let sql_select = "SELECT * FROM " + table;
+    let parameters = [];
+    client.execute(sql_select, parameters, (error, result) => {
+      if (error != undefined) {
+        logger.error("Error:", error);
+        return null;
+      } else {
+        logger.info("Executing a select query " + sql_select);
+        return result.rows;
+      }
+    });
   };
 }
